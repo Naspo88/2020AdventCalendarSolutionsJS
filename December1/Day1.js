ce78bbb0ec1a6c584b6fbd300699a37a03efc222
Roberto Minghi <robertominghi@msn.com>
2020-12-01 15:52:37 +0000

    Incomplete but with test

---
+++ December1/Day1.js
@@ -0,0 +1,46 @@
+const reducerSum = (accumulator, currentValue) => accumulator + currentValue;
+const reducerMult = (accumulator, currentValue) => accumulator * currentValue;
+
+function isSumValue(val, arrayToSum) {
+    const sum = arrayToSum.reduce(reducerSum, 0);
+    return sum === val;
+} 
+
+function getSolution(arrayToMult) {
+    return arrayToMult.reduce(reducerMult, 1);
+}
+
+export function solvePart1 (text) {
+    const array = text.split('\n');
+    let resp;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            const values = [parseInt(array[i]), parseInt(array[j])];
+            if (isSumValue(2020, values)) {
+                resp = getSolution(values);
+                i = array.length;
+                j = array.length;
+            }
+        }
+    }
+    return resp;
+}
+
+export function solvePart2 (text) {
+    const array = text.split('\n');
+    let resp;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            for (let k = j + 1; j < array.length; k++) {
+                const values = [parseInt(array[i]), parseInt(array[j]), parseInt(array[k])];
+                if (isSumValue(2020, values)) {
+                    resp = getSolution(values);
+                    i = array.length;
+                    j = array.length;
+                    k = array.length;
+                }
+            }
+        }
+    }
+    return resp;
+}
