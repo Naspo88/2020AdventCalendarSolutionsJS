30a555834f8feba2c0afa9efbacfe8c9859110d9
Roberto Minghi <robertominghi@msn.com>
2020-12-21 10:10:23 +0000

    Make order

--- src/December21/Day21.js
+++ src/December21/Day21.js
@@ -1,12 +1,12 @@
 import { returnArrayFromText } from '../utils/helpers';
 
 export function solveDay(text, part) {
-  const array = returnArrayFromText(text)
-    .map(val => val.match(/^([a-z\ ]+) \(contains ([a-z,\ ]+)\)$/))
-    .map(val => ({ ingredients: val[1].split(" "), allergens: val[2].split(", ") }));
-  
-  let allergenMap = {};
-  
+	const array = returnArrayFromText(text)
+		.map(val => val.match(/^([a-z\ ]+) \(contains ([a-z,\ ]+)\)$/))
+		.map(val => ({ ingredients: val[1].split(" "), allergens: val[2].split(", ") }));
+	
+	let allergenMap = {};
+	
 	array.forEach(food => {
 		food.allergens.forEach(allergen => {
 			if (allergenMap[allergen] == null) {
@@ -16,47 +16,47 @@ export function solveDay(text, part) {
 					.filter(val => food.ingredients.includes(val));
 			}
 		});
-  });
-  
+	});
+	
 	if (part === 1) {
-    const set = new Set();
-    
+		const set = new Set();
+		
 		Object.values(allergenMap).forEach(allergens => {
 			allergens.forEach(allergen => {
 				set.add(allergen);
 			});
-    });
-    
-    let sum = 0;
-    
+		});
+
+		let sum = 0;
+		
 		array.forEach(food => {
 			sum += food.ingredients.length;
 			set.forEach(allergen => {
 				if (food.ingredients.includes(allergen)) sum--;
 			});
-    });
-    
+		});
+		
 		return sum;
 	} else if (part === 2) {
 		while (true) {
 			Object.keys(allergenMap).forEach(key1 => {
-        let possibilities = [...allergenMap[key1]];
-        
+				let possibilities = [...allergenMap[key1]];
+				
 				Object.keys(allergenMap).forEach(key2 => {
 					if (key2 == key1) return;
 					possibilities = possibilities.filter(value => !allergenMap[key2].includes(value));
-        });
-        
+				});
+				
 				if (possibilities.length >= 1) {
 					allergenMap[key1] = possibilities;
 				}
-      });
-      
+			});
+			
 			if (!Object.values(allergenMap).some(array => array.length > 1)) break;
-    }
-    
-    return Object.keys(allergenMap).sort().map(key => allergenMap[key][0]).join(',');
-  }
-  
+		}
+	
+		return Object.keys(allergenMap).sort().map(key => allergenMap[key][0]).join(',');
+	}
+	
 	return null;
-}
+};
